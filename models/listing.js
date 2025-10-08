eaa8eb0aedf84405d82adaaac21f676286126948
Alok Kumar <2784alok@gmail.com>
2025-10-08 15:42:08 +0530

    added mvc

--- models/listing.js
+++ models/listing.js
@@ -1,7 +1,7 @@
 const { ref } = require("joi");
 const mongoose=require("mongoose");
 const Schema=mongoose.Schema;
-const Review=require("./review.js");
+const Review=require("./review");
 
 const listingSchema= new Schema({
     title:{
--- routes/review.js
+++ routes/review.js
@@ -10,24 +10,15 @@ const {
   isLoggedIn,
   isReviewAuthor,
 } = require("../middleware.js");
+const reviewController=require("../controllers/reviews.js");
 
-//Rreview
+//Review
 //post review route
 router.post(
   "/",
   validateReview,
   isLoggedIn,
-  wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    newReview.author = req.user._id;
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-    req.flash("success", " Review Added!");
-    res.redirect(`/listings/${listing._id}`);
-  })
+  wrapAsync(reviewController.createReview)
 );
 
 //review delete route
@@ -35,13 +26,7 @@ router.delete(
   "/:reviewId",
   isReviewAuthor,
   isLoggedIn,
-  wrapAsync(async (req, res) => {
-    let { id, reviewId } = req.params;
-    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success", "Review Deleted!");
-    res.redirect(`/listings/${id}`);
-  })
+  wrapAsync(reviewController.deleteReview)
 );
 
 module.exports = router;
