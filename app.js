6c6c4366ec0f51866eb15cc91b2a645fa93b9cc2
Alok Kumar <2784alok@gmail.com>
2025-10-12 23:43:01 +0530

    finishing it for publish

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session=require("express-session");
+const MongoStore = require('connect-mongo');
 const flash=require("connect-flash");
 const passport=require("passport");
 const LocalStrategy=require("passport-local");
@@ -21,7 +22,9 @@ const listingRouter=require("./routes/listing.js");
 const reviewRouter=require("./routes/review.js");
 const userRouter=require("./routes/user.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/StayEase";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/StayEase";  //local db
+
+const dbUrl=process.env.ATLASDB_URL;
 
 main().then(() => {
     console.log("connected to DB");
@@ -31,7 +34,7 @@ main().then(() => {
     });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
@@ -41,8 +44,27 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate); //for using same code boilerplate in diiferent pages
 app.use(express.static(path.join(__dirname, "/public"))); //path set for public css 
 
+
+
+
+// app.get("/", (req, res) => {
+//     res.send("root is working");
+// })
+
+const store= MongoStore.create({
+    mongoUrl:dbUrl,
+    crypto:{secret:process.env.SECRET},
+    touchAfter:24*3600,
+
+});
+
+store.on("error", () =>{
+console. log ("ERROR in MONGO SESSION STORE", err);
+})
+
 const sessionOptions={
-    secret:"mysupersecretmessage",
+    store,
+    secret:process.env.SECRET,
     resave:false,
     saveUninitialized:true,
     cookie:{
@@ -51,12 +73,6 @@ const sessionOptions={
         httpOnly:true,
     },
 }
-
-
-app.get("/", (req, res) => {
-    res.send("root is working");
-})
-
 
 app.use(session(sessionOptions));
 app.use(flash());
